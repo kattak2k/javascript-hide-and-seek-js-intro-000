8006b080fe375b239e9f6536e0923d1dfd9d4be2
Ravi <kkiran.ror@gmail.com>
2020-03-15 02:16:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  const firstElement = document.querySelector(selector);
+  return firstElement;
+}
+
+function  nestedTarget(){
+  const firstElement = document.querySelector("#nested .target");
+  return firstElement;
+}
+
+function  deepestChild(){
+  const firstElement = document.querySelector('#grand-node div div div div');
+  return firstElement;
+
+}
+
+function  increaseRankBy(n){
+
+}
